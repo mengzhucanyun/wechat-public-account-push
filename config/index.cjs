9fb8ca1682a132e410d8575e19d5a3be5ee9eea9
HuangJunLin <996718452@qq.com>
2023-08-22 09:22:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '黄俊林的大宝贝儿',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU24398TFa9A8FwQx5vYM6nu70rJdrpmmPrnAf77',
+      id: 'PDU24967TxuZySlhRKwA7lzKZqigQcDudmfmatiV8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
